de98146052f97698533057a598becb66db8fa570
hbgamer555 <58474589+hbgamer555@users.noreply.github.com>
2021-05-28 19:18:41 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,10 @@
+def swapFileData():
+    data_a=open(r'C:\Users\ADMIN\New folder\essay.txt',"w")
+    data_b=open(r"C:\Users\ADMIN\New folder\trick.txt","w")
+    data_a.write("you got tricked")
+    data_b.write("this is an essay")
+
+
+
+
+swapFileData()
